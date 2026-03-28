2927132be2c069b6a52680165a2f0bee4aa97a60
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2026-03-27 20:25:08 -0400

    add general structure for monster list

--- src/MonsterHunterTracker/Form1.Designer.cs
+++ src/MonsterHunterTracker/Form1.Designer.cs
@@ -33,29 +33,28 @@ partial class Form1
         lblGameName = new System.Windows.Forms.Label();
         gbGame = new System.Windows.Forms.GroupBox();
         gbMonsterList = new System.Windows.Forms.GroupBox();
+        btnDecrementCount = new System.Windows.Forms.Button();
+        lblHunted = new System.Windows.Forms.Label();
+        btnIncrementCount = new System.Windows.Forms.Button();
         btnClear = new System.Windows.Forms.Button();
+        txtMonsterCounts = new System.Windows.Forms.TextBox();
         btnRemove = new System.Windows.Forms.Button();
         btnAddToMonsterList = new System.Windows.Forms.Button();
         lstMonsterList = new System.Windows.Forms.ListBox();
         txtMonsterListSearch = new System.Windows.Forms.TextBox();
-        groupBox1 = new System.Windows.Forms.GroupBox();
-        btnDecrementCount = new System.Windows.Forms.Button();
-        btnIncrementCount = new System.Windows.Forms.Button();
-        txtMonsterCounts = new System.Windows.Forms.TextBox();
         groupMonsterList = new System.Windows.Forms.GroupBox();
         gbGame.SuspendLayout();
         gbMonsterList.SuspendLayout();
-        groupBox1.SuspendLayout();
         groupMonsterList.SuspendLayout();
         SuspendLayout();
         // 
         // lblGameName
         // 
         lblGameName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        lblGameName.Location = new System.Drawing.Point(9, 31);
+        lblGameName.Location = new System.Drawing.Point(3, 31);
         lblGameName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
         lblGameName.Name = "lblGameName";
-        lblGameName.Size = new System.Drawing.Size(212, 20);
+        lblGameName.Size = new System.Drawing.Size(271, 20);
         lblGameName.TabIndex = 0;
         lblGameName.Text = "Monster Hunter Generations Ultimate";
         // 
@@ -64,16 +63,20 @@ partial class Form1
         gbGame.Controls.Add(lblGameName);
         gbGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
         gbGame.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        gbGame.Location = new System.Drawing.Point(28, 24);
+        gbGame.Location = new System.Drawing.Point(12, 3);
         gbGame.Name = "gbGame";
-        gbGame.Size = new System.Drawing.Size(234, 61);
+        gbGame.Size = new System.Drawing.Size(281, 61);
         gbGame.TabIndex = 2;
         gbGame.TabStop = false;
         gbGame.Text = "Game";
         // 
         // gbMonsterList
         // 
+        gbMonsterList.Controls.Add(btnDecrementCount);
+        gbMonsterList.Controls.Add(lblHunted);
+        gbMonsterList.Controls.Add(btnIncrementCount);
         gbMonsterList.Controls.Add(btnClear);
+        gbMonsterList.Controls.Add(txtMonsterCounts);
         gbMonsterList.Controls.Add(btnRemove);
         gbMonsterList.Controls.Add(btnAddToMonsterList);
         gbMonsterList.Controls.Add(lstMonsterList);
@@ -81,30 +84,75 @@ partial class Form1
         gbMonsterList.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
         gbMonsterList.Location = new System.Drawing.Point(16, 22);
         gbMonsterList.Name = "gbMonsterList";
-        gbMonsterList.Size = new System.Drawing.Size(403, 266);
+        gbMonsterList.Size = new System.Drawing.Size(380, 347);
         gbMonsterList.TabIndex = 3;
         gbMonsterList.TabStop = false;
         gbMonsterList.Text = "Monster List";
+        // 
+        // btnDecrementCount
+        // 
+        btnDecrementCount.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        btnDecrementCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+        btnDecrementCount.Location = new System.Drawing.Point(316, 101);
+        btnDecrementCount.Name = "btnDecrementCount";
+        btnDecrementCount.Size = new System.Drawing.Size(58, 40);
+        btnDecrementCount.TabIndex = 2;
+        btnDecrementCount.Text = "-";
+        btnDecrementCount.UseVisualStyleBackColor = false;
+        // 
+        // lblHunted
+        // 
+        lblHunted.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        lblHunted.Location = new System.Drawing.Point(240, 72);
+        lblHunted.Name = "lblHunted";
+        lblHunted.Size = new System.Drawing.Size(62, 23);
+        lblHunted.TabIndex = 5;
+        lblHunted.Text = "Hunted";
+        // 
+        // btnIncrementCount
+        // 
+        btnIncrementCount.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        btnIncrementCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+        btnIncrementCount.ForeColor = System.Drawing.SystemColors.ButtonFace;
+        btnIncrementCount.Location = new System.Drawing.Point(240, 101);
+        btnIncrementCount.Name = "btnIncrementCount";
+        btnIncrementCount.Size = new System.Drawing.Size(58, 40);
+        btnIncrementCount.TabIndex = 1;
+        btnIncrementCount.Text = "+";
+        btnIncrementCount.UseVisualStyleBackColor = false;
         // 
         // btnClear
         // 
         btnClear.BackColor = System.Drawing.SystemColors.ControlDarkDark;
         btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        btnClear.Location = new System.Drawing.Point(273, 35);
+        btnClear.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        btnClear.Location = new System.Drawing.Point(205, 34);
         btnClear.Name = "btnClear";
-        btnClear.Size = new System.Drawing.Size(29, 34);
+        btnClear.Size = new System.Drawing.Size(29, 29);
         btnClear.TabIndex = 4;
         btnClear.Text = "X";
         btnClear.UseVisualStyleBackColor = false;
+        // 
+        // txtMonsterCounts
+        // 
+        txtMonsterCounts.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        txtMonsterCounts.Location = new System.Drawing.Point(316, 69);
+        txtMonsterCounts.MaxLength = 3;
+        txtMonsterCounts.Name = "txtMonsterCounts";
+        txtMonsterCounts.ReadOnly = true;
+        txtMonsterCounts.Size = new System.Drawing.Size(58, 29);
+        txtMonsterCounts.TabIndex = 0;
+        txtMonsterCounts.Text = "999";
+        txtMonsterCounts.WordWrap = false;
         // 
         // btnRemove
         // 
         btnRemove.BackColor = System.Drawing.SystemColors.ControlDarkDark;
         btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
         btnRemove.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        btnRemove.Location = new System.Drawing.Point(316, 120);
+        btnRemove.Location = new System.Drawing.Point(293, 34);
         btnRemove.Name = "btnRemove";
-        btnRemove.Size = new System.Drawing.Size(81, 34);
+        btnRemove.Size = new System.Drawing.Size(81, 29);
         btnRemove.TabIndex = 3;
         btnRemove.Text = "Remove";
         btnRemove.UseVisualStyleBackColor = false;
@@ -113,15 +161,17 @@ partial class Form1
         // 
         btnAddToMonsterList.BackColor = System.Drawing.SystemColors.ControlDarkDark;
         btnAddToMonsterList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        btnAddToMonsterList.Location = new System.Drawing.Point(315, 79);
+        btnAddToMonsterList.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
+        btnAddToMonsterList.Location = new System.Drawing.Point(240, 34);
         btnAddToMonsterList.Name = "btnAddToMonsterList";
-        btnAddToMonsterList.Size = new System.Drawing.Size(82, 35);
+        btnAddToMonsterList.Size = new System.Drawing.Size(48, 29);
         btnAddToMonsterList.TabIndex = 2;
         btnAddToMonsterList.Text = "Add";
         btnAddToMonsterList.UseVisualStyleBackColor = false;
         // 
         // lstMonsterList
         // 
+        lstMonsterList.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
         lstMonsterList.FormattingEnabled = true;
         lstMonsterList.Items.AddRange(new object[]
         {
@@ -130,69 +180,23 @@ partial class Form1
 
         lstMonsterList.Location = new System.Drawing.Point(6, 75);
         lstMonsterList.Name = "lstMonsterList";
-        lstMonsterList.Size = new System.Drawing.Size(303, 184);
+        lstMonsterList.Size = new System.Drawing.Size(228, 256);
         lstMonsterList.TabIndex = 1;
         // 
         // txtMonsterListSearch
         // 
+        txtMonsterListSearch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
         txtMonsterListSearch.Location = new System.Drawing.Point(6, 34);
         txtMonsterListSearch.Name = "txtMonsterListSearch";
-        txtMonsterListSearch.Size = new System.Drawing.Size(263, 35);
+        txtMonsterListSearch.Size = new System.Drawing.Size(197, 29);
         txtMonsterListSearch.TabIndex = 0;
-        // 
-        // groupBox1
-        // 
-        groupBox1.Controls.Add(btnDecrementCount);
-        groupBox1.Controls.Add(btnIncrementCount);
-        groupBox1.Controls.Add(txtMonsterCounts);
-        groupBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-        groupBox1.Location = new System.Drawing.Point(425, 32);
-        groupBox1.Name = "groupBox1";
-        groupBox1.Size = new System.Drawing.Size(200, 154);
-        groupBox1.TabIndex = 4;
-        groupBox1.TabStop = false;
-        groupBox1.Text = "Hunted";
-        // 
-        // btnDecrementCount
-        // 
-        btnDecrementCount.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-        btnDecrementCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        btnDecrementCount.Location = new System.Drawing.Point(114, 93);
-        btnDecrementCount.Name = "btnDecrementCount";
-        btnDecrementCount.Size = new System.Drawing.Size(80, 40);
-        btnDecrementCount.TabIndex = 2;
-        btnDecrementCount.Text = "-";
-        btnDecrementCount.UseVisualStyleBackColor = false;
-        // 
-        // btnIncrementCount
-        // 
-        btnIncrementCount.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-        btnIncrementCount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-        btnIncrementCount.ForeColor = System.Drawing.SystemColors.ButtonFace;
-        btnIncrementCount.Location = new System.Drawing.Point(8, 93);
-        btnIncrementCount.Name = "btnIncrementCount";
-        btnIncrementCount.Size = new System.Drawing.Size(80, 40);
-        btnIncrementCount.TabIndex = 1;
-        btnIncrementCount.Text = "+";
-        btnIncrementCount.UseVisualStyleBackColor = false;
-        // 
-        // txtMonsterCounts
-        // 
-        txtMonsterCounts.Location = new System.Drawing.Point(11, 34);
-        txtMonsterCounts.MaxLength = 10;
-        txtMonsterCounts.Name = "txtMonsterCounts";
-        txtMonsterCounts.ReadOnly = true;
-        txtMonsterCounts.Size = new System.Drawing.Size(183, 35);
-        txtMonsterCounts.TabIndex = 0;
-        txtMonsterCounts.WordWrap = false;
         // 
         // groupMonsterList
         // 
         groupMonsterList.Controls.Add(gbMonsterList);
-        groupMonsterList.Controls.Add(groupBox1);
-        groupMonsterList.Location = new System.Drawing.Point(12, 91);
+        groupMonsterList.Location = new System.Drawing.Point(12, 70);
         groupMonsterList.Name = "groupMonsterList";
-        groupMonsterList.Size = new System.Drawing.Size(656, 297);
+        groupMonsterList.Size = new System.Drawing.Size(656, 375);
         groupMonsterList.TabIndex = 5;
         groupMonsterList.TabStop = false;
         // 
@@ -213,11 +217,11 @@ partial class Form1
         gbGame.ResumeLayout(false);
         gbMonsterList.ResumeLayout(false);
         gbMonsterList.PerformLayout();
-        groupBox1.ResumeLayout(false);
-        groupBox1.PerformLayout();
         groupMonsterList.ResumeLayout(false);
         ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Label lblHunted;
 
     private System.Windows.Forms.GroupBox groupMonsterList;
 
@@ -231,7 +235,6 @@ partial class Form1
 
     private System.Windows.Forms.Button btnIncrementCount;
 
-    private System.Windows.Forms.GroupBox groupBox1;
     private System.Windows.Forms.TextBox txtMonsterCounts;
 
     private System.Windows.Forms.TextBox txtMonsterListSearch;
