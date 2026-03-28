10c669b1b099319065432a07742e70b89319bb4e
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2026-03-27 21:06:21 -0400

    add logic project to support the ui

---
+++ src/MonsterHunterTracker.Logic/Drops.cs
@@ -0,0 +1,7 @@
+﻿namespace MonsterHunterTracker.Logic;
+
+public class Drops
+{
+    public string Name { get; set; }
+    public Rank Rank { get; set; }
+}
---
+++ src/MonsterHunterTracker.Logic/Monster.cs
@@ -0,0 +1,13 @@
+﻿namespace MonsterHunterTracker.Logic;
+
+public class Monster
+{
+    public string Name { get; set; }
+
+    public string Classification { get; set; }
+    public List<Attributes> Weaknesses { get; set; }
+    public List<Attributes> Strengths { get; set; }
+    public int NumberHunted { get; set; }
+
+    public List<Drops> Drops{ get; set; }
+}
